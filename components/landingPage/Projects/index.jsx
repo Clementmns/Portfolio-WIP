a6e3dc05f831612ae35eee1e49cbf13d56e6db85
Clément Omnès <clementgoni@gmail.com>
2023-11-23 22:20:49 +0100

    Fix bug

--- components/landingPage/Projects/index.jsx
+++ components/landingPage/Projects/index.jsx
@@ -58,11 +58,6 @@ function LandingProjects() {
                animation: animation,
                scrub: true,
                ease: "power.inOut",
-               snap: {
-                  duration: { min: 0.5, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-                  delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-                  ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
-               },
             });
          });
 
